a195c388114ff636af5df10e269912761a5e3459
潘宇 <12275622+pan-yu-peter@user.noreply.gitee.com>
2025-03-25 00:34:24 +0800

    feat: bash init

---
+++ back.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORK_PATH='/peter/projects/devops-backend'
+cd WORK_PATH
+echo "clear old code"
+git reset --hard origin/main
+git clean -f
+echo "pull new code"
+git pull origin main
+echo "start building"
+docker build -t devops-backend .
+echo "stop old container and remove"
+docker stop devops-backend-container
+docker rm devops-backend-container
+echo "start a new container"
+docker container run -p 3000:3000 --name devops-backend-container -d devops-backend
--- web-hook.js
+++ web-hook.js
@@ -1,8 +1,30 @@
 const http = require('http')
+const crypto = require('crypto')
+const SECRETE = '123456' // 验证密码
+
+function sign(body) {
+    return `sha1=` + crypto.createHmac('sha1', SECRETE).update(body).digest('hex');
+}
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
     if(req.method == 'POST' && req.url == '/webhook'){
+        let buffers = []
+        
+        req.on('data', (buffer) => {
+            buffers.push(buffer);
+        })
+
+        req.on('end', () => {
+            let body = Buffer.concat(buffers);
+            let event = req.headers['x-gitHub-event'];
+            let signature = req.headers['x-hub-signature'];
+
+            if(signature !== sign(body)){
+                return res.end('Not Allow');
+            }
+        })
+
         // 给github服务器返回一个响应
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({
