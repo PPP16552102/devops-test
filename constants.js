a05ff044c64e2c3d1e11db8efaaf66e261d4db7c
潘宇 <12275622+pan-yu-peter@user.noreply.gitee.com>
2025-03-25 01:51:09 +0800

    feat: auto sh

---
+++ constants.js
@@ -0,0 +1,8 @@
+const REPO_SHELL_MAP = {
+    'devops-front-react': 'front.sh',
+    'devops-backend': 'back.sh'
+}
+
+module.exports = {
+    REPO_SHELL_MAP
+}
--- web-hook.js
+++ web-hook.js
@@ -1,5 +1,9 @@
 const http = require('http')
 const crypto = require('crypto')
+const { spawn } = require('child_process')
+
+const { REPO_SHELL_MAP } = require('./constants')
+
 const SECRETE = '123456' // 验证密码
 
 function sign(body) {
@@ -17,19 +21,38 @@ const server = http.createServer((req, res) => {
 
         req.on('end', () => {
             let body = Buffer.concat(buffers);
-            let event = req.headers['x-gitHub-event'];
+            let event = req.headers['x-github-event'];
             let signature = req.headers['x-hub-signature'];
 
             if(signature !== sign(body)){
                 return res.end('Not Allow');
             }
-        })
 
-        // 给github服务器返回一个响应
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            ok: true
-        }))
+            // 给github服务器返回一个响应
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                ok: true
+            }))
+
+            // 如果是push，开始部署
+            if(event == 'push'){
+                let payload = JSON.parse(body);
+                // 根据仓库名称执行对应的sh脚本
+                if(payload.repository.name && REPO_SHELL_MAP[payload.repository.name]){
+                    let child = spawn('sh', [`./${REPO_SHELL_MAP[payload.repository.name]}`]);
+                    let buffers = [];
+                    child.stdout.on('data', (buffer) => {
+                        buffers.push(buffer);
+                    })
+
+                    child.stdout.on('end', (buffer) => {
+                        let logText = Buffer.concat(buffers);
+                        console.log(logText);
+                        console.log('build success');
+                    })
+                }
+            }
+        })
     }else{
         res.end('Not Found');
     }
