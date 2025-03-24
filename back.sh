5cc5a4c2f14ad8ef7baed3ae06f1fc4d7a4ab64c
潘宇 <12275622+pan-yu-peter@user.noreply.gitee.com>
2025-03-25 00:47:36 +0800

    feat: build version

--- back.sh
+++ back.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "pull new code"
 git pull origin main
 echo "start building"
-docker build -t devops-backend .
+docker build -t devops-backend:1.0 .
 echo "stop old container and remove"
 docker stop devops-backend-container
 docker rm devops-backend-container
 echo "start a new container"
-docker container run -p 3000:3000 --name devops-backend-container -d devops-backend
+docker container run -p 3000:3000 --name devops-backend-container -d devops-backend:1.0
